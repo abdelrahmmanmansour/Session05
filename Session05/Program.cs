db7c226389d1fdb733bc83bc3e3f4067e45dfcd0
Abdelrahman Mansour741 <abdo.mansour741@gmail.com>
2025-07-12 17:35:15 +0300

    Assignmennt05

--- Session05/Program.cs
+++ Session05/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
+
+            #region Q1
             Console.WriteLine("Hello,Assignment05");
+            #endregion
+            #region Q2
+            Console.WriteLine("Hello");
+            #endregion
         }
     }
 }
